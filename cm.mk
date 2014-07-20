763cb7fc76f244000c73f4b9dcb6492f4711a571
thegb <the_guardian_blinded@hotmail.com>
2014-07-20 04:00:13 -0700

    P880: Product model correction
    In most cm devices, the product model is the full name of the device.
    This commit updates the (not nice) LG-P880 to Optimus 4X HD.
    
    Change-Id: I2eef02abcd24bbe3247dd7b61ffd1132e2d76db2

--- cm.mk
+++ cm.mk
@@ -18,6 +18,7 @@ $(call inherit-product, device/lge/p880/p880.mk)
 PRODUCT_DEVICE := p880
 PRODUCT_NAME := cm_p880
 PRODUCT_BRAND := lge
+PRODUCT_MODEL := Optimus 4X HD
 PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=x3_open_eu BUILD_FINGERPRINT=lge/x3_open_eu/x3:4.1.2/JZO54K/P88020a.1e56bb4c69:user/release-keys PRIVATE_BUILD_DESC="x3_open_eu-user 4.1.2 JZO54K P88020a.1e56bb4c69 release-keys"
